c5c93f884f83d00ff0852111cfe2b2d0ff5afe2e
Catpute <59244429+Catpute@users.noreply.github.com>
2022-02-14 18:29:44 -0800

    Update party.js

--- party.js
+++ party.js
@@ -1,21 +1,21 @@
-if(PartyTime === undefined) var PartyTime = {};
+if(party === undefined) var party = {};
 if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 
-MyMod.launch = function(){
-PartyTime.isLoaded = 1;
+party.launch = function(){
+party.isLoaded = 1;
 Game.customOptionsMenu.push(function(){
 	CCSE.AppendCollapsibleOptionsMenu("Party Button", '<button onclick="lol()"></button>');
 });
 }
 
-if(!PartyTime.isLoaded){
+if(!party.isLoaded){
 	if(CCSE && CCSE.isLoaded){
-		PartyTime.launch();
+		party.launch();
 	}
 	else{
 		if(!CCSE) var CCSE = {};
 		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
-		CCSE.postLoadHooks.push(PartyTime.launch);
+		CCSE.postLoadHooks.push(party.launch);
 	}
   function lol()
   {
